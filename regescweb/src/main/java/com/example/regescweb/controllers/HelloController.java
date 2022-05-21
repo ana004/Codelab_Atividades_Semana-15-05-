e5e5d81ac3757b7776bde3206f61dc5e378a241d
ana004 <paulaana08002019@gmail.com>
2022-05-21 17:10:44 -0300

    Adicionados metodos utilizando o thymeleaf para exibir conteudo em arquivo html

--- regescweb/src/main/java/com/example/regescweb/controllers/HelloController.java
+++ regescweb/src/main/java/com/example/regescweb/controllers/HelloController.java
@@ -1,13 +1,30 @@
 package com.example.regescweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class HelloController {
-
     @GetMapping("/hello")
-    public String hello() {
-        return "hello"; // o Spring vai renderizar o arquivo templates/hello.html
+    public ModelAndView hello() {
+        ModelAndView mv = new ModelAndView("hello");
+        mv.addObject("nome", "Maria!");
+        return mv; //Spring vai renderizar o arquivo templates/hello.html
+    }
+    @GetMapping("/hello-model")
+    public String hello(Model model) {
+        model.addAttribute("nome", "Zezinho!");
+        return "hello"; //Spring vai renderizar o arquivo templates/hello.html
+    }
+
+    @GetMapping("/hello-servlet")
+    public String hello(HttpServletRequest  request) {
+        request.setAttribute("nome", "Ana");
+        return "hello"; //Spring vai renderizar o arquivo templates/hello.html
     }
 }
