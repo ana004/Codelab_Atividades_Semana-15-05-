c65fde226a48617cb65d6734d75003f8a2c67b48
ana004 <paulaana08002019@gmail.com>
2022-05-23 11:48:12 -0300

    Adicionado um formulario de criacao com o uso de Thymeleaf

--- regescweb/src/main/java/com/example/regescweb/controllers/ProfessorController.java
+++ regescweb/src/main/java/com/example/regescweb/controllers/ProfessorController.java
@@ -2,13 +2,14 @@ package com.example.regescweb.controllers;
 
 
 import com.example.regescweb.models.Professor;
-
+import com.example.regescweb.models.StatusProfessor;
 import com.example.regescweb.repositories.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-import java.util.*;
+
+import java.util.List;
 
 
 @Controller
@@ -21,6 +22,14 @@ public class ProfessorController {
         List<Professor> professores = this.professorRepository.findAll();
         ModelAndView mv = new ModelAndView("professores/index");
         mv.addObject("professores", professores);
+
+        return mv;
+    }
+
+    @GetMapping("/professor/new")
+    public ModelAndView nnew() {
+        ModelAndView mv = new ModelAndView("professores/new");
+        mv.addObject("statusProfessor", StatusProfessor.values());
 
         return mv;
     }
