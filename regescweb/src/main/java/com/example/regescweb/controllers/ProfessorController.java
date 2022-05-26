1556f39e9138382d7be5e54392d038ca656b7bea
ana004 <paulaana08002019@gmail.com>
2022-05-26 14:36:40 -0300

    Criacao de Pagina de Detalhes para entidades

--- regescweb/src/main/java/com/example/regescweb/controllers/ProfessorController.java
+++ regescweb/src/main/java/com/example/regescweb/controllers/ProfessorController.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -50,8 +52,25 @@ public class ProfessorController {
             Professor professor = requisicao.toProfessor();
             this.professorRepository.save(professor);
 
+            return new ModelAndView("redirect:/professores/" + professor.getId());
+        }
+    }
+    @GetMapping("/professores/{id}")
+    public ModelAndView show(@PathVariable Long id) {
+        Optional<Professor> optional = this.professorRepository.findById(id);
+
+        if (optional.isPresent()) {
+            Professor professor = optional.get();
+
+            ModelAndView mv = new ModelAndView("professores/show");
+            mv.addObject("professor", professor);
+
+            return mv;
+        }
+        // não achou um registro na tabela Professor com o id informado
+        else {
+            System.out.println("$$$$$$$$$$$$$ NÃO ACHOU O PROFESSOR DE ID" + id + "$$$$$$$$$$$$$");
             return new ModelAndView("redirect:/professores");
         }
-
     }
 }
