fe5a89493ce0f301713d5ddddacf2c42a61ea790
ana004 <paulaana08002019@gmail.com>
2022-05-29 18:19:32 -0300

    Adicionada a funcao de delecao do registros com tratamento de erro

--- regescweb/src/main/java/com/example/regescweb/controllers/ProfessorController.java
+++ regescweb/src/main/java/com/example/regescweb/controllers/ProfessorController.java
@@ -6,6 +6,7 @@ import com.example.regescweb.models.Professor;
 import com.example.regescweb.models.StatusProfessor;
 import com.example.regescweb.repositories.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -118,6 +119,17 @@ public class ProfessorController {
                 System.out.println("############# NÃO ACHOU O PROFESSOR DE ID" + id + "################");
                 return new ModelAndView("redirect:/professores");
             }
+        }
+    }
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable Long id) {
+        try {
+            this.professorRepository.deleteById(id);
+            return "redirect:/professores";
+        }
+        catch(EmptyResultDataAccessException e){
+            System.out.println(e);
+            return "redirect:/professores";
         }
     }
 }
